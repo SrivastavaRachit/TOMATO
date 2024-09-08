51554452cd7da54ddf27a3b65bf3c53eab942eac
SrivastavaRachit <87166442+SrivastavaRachit@users.noreply.github.com>
2024-09-09 00:02:06 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://rachitsrivastava0406:rs6518619@cluster0.dn9jrpd.mongodb.net/TOMATO')
+    await mongoose.connect('mongodb+srv://<email>:<password>@cluster0.dn9jrpd.mongodb.net/TOMATO')
     .then (() => console.log("DB CONNECTED"));
 }
